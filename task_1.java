1aba411bd52e8a13bd74e490bafa645dc57f5c50
praveen0509 <praveen.pavan0509@gmail.com>
2018-11-16 18:06:58 +0530

    commit chanes

--- task_1.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class task_1
-{
- public static void main(String args[])
- {
-  System.out.println("task has been completed");
- }
-}
--- task_2.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-public class task_2
-{
- public static void main(String args[])
- {
-  System.out.println("task-2 has been completed");
- }
-}
